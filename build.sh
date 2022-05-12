9cb9955bb7f3f91e720e61108d066a5149a0e45c
Jyotiraditya Panda <jyotiraditya@aospa.co>
2022-05-12 14:00:19 +0530

    build.sh: Allow passing HOME_DIR and TC_DIR.
    also use https://oshi.at/ & pass OUT_DIR
    
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,54 @@
-#!/bin/bash
+ #!/bin/bash
 #
 # Compile script for QuicksilveR kernel
 # Copyright (C) 2020-2021 Adithya R.
+
+# Setup getopt.
+long_opts="regen,clean,homedir:,tcdir:"
+getopt_cmd=$(getopt -o rch:t: --long "$long_opts" \
+            -n $(basename $0) -- "$@") || \
+            { echo -e "\nError: Getopt failed. Extra args\n"; exit 1;}
+
+eval set -- "$getopt_cmd"
+
+while true; do
+    case "$1" in
+        -r|--regen|r|regen) FLAG_REGEN_DEFCONFIG=y;;
+        -c|--clean|c|clean) FLAG_CLEAN_BUILD=y;;
+        -h|--homedir|h|homedir) HOME_DIR="$2"; shift;;
+        -t|--tcdir|t|tcdir) TC_DIR="$2"; shift;;
+        -o|--outdir|o|outdir) OUT_DIR="$2"; shift;;
+        --) shift; break;;
+    esac
+    shift
+done
+
+# Setup HOME dir
+if [ $HOME_DIR ]; then
+    HOME_DIR=$HOME_DIR
+else
+    HOME_DIR=$HOME
+fi
+echo -e "HOME directory is at $HOME_DIR\n"
+
+# Setup Toolchain dir
+if [ $TC_DIR ]; then
+    TC_DIR="$HOME_DIR/$TC_DIR"
+else
+    TC_DIR="$HOME_DIR/tc"
+fi
+echo -e "Toolchain directory is at $TC_DIR\n"
+
+# Setup OUT dir
+if [ $OUT_DIR ]; then
+    OUT_DIR=$OUT_DIR
+else
+    OUT_DIR=out
+fi
+echo -e "Out directory is at $OUT_DIR\n"
+
+export KBUILD_BUILD_USER=007
+export KBUILD_BUILD_HOST=skyfall
 
 SECONDS=0 # builtin bash timer
 ZIPNAME="QuicksilveR-drg-$(date '+%Y%m%d-%H%M').zip"
@@ -9,48 +56,49 @@ if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
    head=$(git rev-parse --verify HEAD 2>/dev/null); then
         ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
 fi
-TC_DIR="$HOME/tc/clang-r416183b1"
-GCC_64_DIR="$HOME/tc/aarch64-linux-android-4.9"
-GCC_32_DIR="$HOME/tc/arm-linux-androideabi-4.9"
+CLANG_DIR="$TC_DIR/clang-r416183b1"
+GCC_64_DIR="$TC_DIR/aarch64-linux-android-4.9"
+GCC_32_DIR="$TC_DIR/arm-linux-androideabi-4.9"
 DEFCONFIG="vendor/drg-perf_defconfig"
 
-export KBUILD_BUILD_USER=007
-export KBUILD_BUILD_HOST=skyfall
-export PATH="$TC_DIR/bin:$PATH"
+export PATH="$CLANG_DIR/bin:$PATH"
 
-if [[ $1 = "-c" || $1 = "--clean" ]]; then
-	rm -rf out
-fi
-
-if [[ $1 = "-r" || $1 = "--regen" ]]; then
-	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-	cp out/defconfig arch/arm64/configs/$DEFCONFIG
+# Regenerate defconfig, if requested so
+if [ "$FLAG_REGEN_DEFCONFIG" = 'y' ]; then
+	 make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+	cp $OUT_DIR/defconfig arch/arm64/configs/$DEFCONFIG
 	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
 	exit 1
 fi
 
-mkdir -p out
-make O=out ARCH=arm64 $DEFCONFIG
+# Prep for a clean build, if requested so
+if [ "$FLAG_CLEAN_BUILD" = 'y' ]; then
+	echo -e "\nCleaning output folder..."
+	rm -rf $OUT_DIR
+fi
+
+mkdir -p $OUT_DIR
+make O=$OUT_DIR ARCH=arm64 $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
-make -j"$(nproc --all)" O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- Image.gz-dtb
+make -j"$(nproc --all)" O=$OUT_DIR ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- Image.gz-dtb
 
-if [ -f "out/arch/arm64/boot/Image.gz-dtb" ]; then
+if [ -f "$OUT_DIR/arch/arm64/boot/Image.gz-dtb" ]; then
 	echo -e "\nKernel compiled succesfully! Zipping up...\n"
 	if ! git clone -q https://github.com/BladeRunner-A2C/AnyKernel3 -b drg; then
 		echo -e "\nCloning AnyKernel3 repo failed! Aborting..."
 		exit 1
 	fi
-	cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+	cp $OUT_DIR/arch/arm64/boot/Image.gz-dtb AnyKernel3
 	rm -f ./*zip
 	cd AnyKernel3 || exit
-	rm -rf out/arch/arm64/boot
+	rm -rf $OUT_DIR/arch/arm64/boot
 	zip -r9 "../$ZIPNAME" ./* -x '*.git*' README.md ./*placeholder
 	cd ..
 	rm -rf AnyKernel3
 	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
 	echo "Zip: $ZIPNAME"
-	curl -# -F "file=@${ZIPNAME}" https://0x0.st
+	curl -F "file=@${ZIPNAME}" https://oshi.at
 	echo
 else
 	echo -e "\nCompilation failed!"
